177012189c3b3f01fec5be0245b9aad53709b4ab
Emilian Roman <roman.emilian@outlook.com>
2020-11-03 06:05:49 +0800

    Introduce the Campaign PDO
    This commit introduces the plain data object version of the Campaign
    entity. As part of making HXE much more data-driven, this object only
    exposes properties that represent a HCE/SPV3 campaign with Missions and
    Difficulties.
    
    In the real world, this object will be hydrated through by deserialising
    an XML file from the filesystem.
    
    (cherry picked from commit c43ac322aebbdc96a6caadb656afe028414930d6)

---
+++ src/Campaign.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace HXE
+{
+  /// <summary>
+  ///   Resume-aware Campaign object.
+  /// </summary>
+  public class Campaign
+  {
+    public CampaignResume   Resume       { get; set; } /* awareness of campaign resuming      */
+    public List<Mission>    Missions     { get; set; } /* available missions for resuming     */
+    public List<Difficulty> Difficulties { get; set; } /* available difficulties for resuming */
+
+    /// <summary>
+    ///   Object representing the variable and possibility of Campaign resuming.
+    /// </summary>
+    public class CampaignResume
+    {
+      public bool   Enabled  { get; set; } /* project supports campaign resuming */
+      public string Variable { get; set; } /* stores current campaign mission id */
+    }
+  }
+}
---
+++ src/Difficulty.cs
@@ -0,0 +1,11 @@
+﻿namespace HXE
+{
+  /// <summary>
+  ///   Campaign Difficulty object.
+  /// </summary>
+  public class Difficulty
+  {
+    public int    ID   { get; set; } /* id representing difficulty as per savegame.bin specification  */
+    public string Name { get; set; } /* human-readable name of the difficulty, e.g. Easy, Noble, etc. */
+  }
+}
