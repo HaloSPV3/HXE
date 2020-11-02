aaaf86ba441626c309e00827cc13fa9ab79f7eea
Emilian Roman <roman.emilian@outlook.com>
2020-11-03 06:12:56 +0800

    Introduce the Configuration PDO
    This commit introduces the plain data object version of the
    Configuration entity. As part of making HXE much more data-driven, this
    object only exposes properties that represent available settings unique
    to SPV3.2 or SPV3.3, e.g. post-processing and player tweaks.
    
    In the real world, this object will be hydrated through by deserialising
    an XML file from the filesystem. Example of instantiation:
    
        new Configuration
        {
          ID = Guid.NewGuid().ToString(),
          Name = "Cinematic bars", Variable = "set loud_dialog_hack",
          Options = new List<Configuration.ConfigurationOption>
          {
            new Configuration.ConfigurationOption
            {
              Name = "On",
              Value  = "1",
              Default = true
            },
            new Configuration.ConfigurationOption
            {
              Name = "Off",
              Value = "0"
            },
          }
        },

---
+++ src/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace HXE
+{
+  /// <summary>
+  ///   Configuration object.
+  /// </summary>
+  public class Configuration
+  {
+    public string                    ID       { get; set; } /* unique id used for user preference persistence         */
+    public string                    Name     { get; set; } /* human-readable name of the configuration               */
+    public string                    Variable { get; set; } /* representative variable/command in the initiation file */
+    public List<ConfigurationOption> Options  { get; set; } /* list of available options for this configuration       */
+
+    public class ConfigurationOption
+    {
+      public string Name    { get; set; } /* human readable version of this option                                   */
+      public string Value   { get; set; } /* value to be assigned to the representative variable in the file         */
+      public bool   Default { get; set; } /* declares this option to be the default one when the user hasn't set one */
+    }
+  }
+}
