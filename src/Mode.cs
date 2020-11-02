20be8a4cecab869384c66db934519970fb34b787
Emilian Roman <roman.emilian@outlook.com>
2020-11-03 06:20:53 +0800

    Introduce the Kernel Mode PDO
    This commit introduces the Kernel Mode PDO, which encapsulates the
    Campaign and Configuration objects.
    
    Thus, the Mode can represent an entire project such as the SPV3 mod.
    Such projects would have a dedicated Mode object exposing the Missions,
    Difficulties and Configuration options they offer.
    
    The HXE Kernel would adapt accordingly to the declared values and handle
    each option as specified.
    
    (cherry picked from commit ad83b6dedb0ff9197aca6bdb77947b189d895e59)

---
+++ src/Mode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace HXE
+{
+  /// <summary>
+  ///   HXE Kernel Mode object.
+  /// </summary>
+  public class Mode
+  {
+    public string              ID             { get; set; } /* id of the mode for subsequent lookup */
+    public string              Name           { get; set; } /* human-readable name of the mode      */
+    public Campaign            Campaign       { get; set; } /* associated campaign object           */
+    public List<Configuration> Configurations { get; set; } /* associated configuration object      */
+  }
+}
