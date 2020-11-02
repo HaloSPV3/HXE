66a819070f81b4e8556037728375e3efd3b62d31
Emilian Roman <roman.emilian@outlook.com>
2020-11-03 06:05:49 +0800

    Introduce the Campaign PDO
    This commit introduces the plain data object version of the Campaign
    entity. As part of making HXE much more data-driven, this object only
    exposes properties that represent a HCE/SPV3 campaign with Missions and
    Difficulties.
    
    In the real world, this object will be hydrated through by deserialising
    an XML file from the filesystem.

---
+++ src/Mission.cs
@@ -0,0 +1,12 @@
+﻿namespace HXE
+{
+  /// <summary>
+  ///   Campaign Mission object.
+  /// </summary>
+  public class Mission
+  {
+    public int    ID          { get; set; } /* id recognised by ui.map for campaign resuming, passed by hxe to init */
+    public string Name        { get; set; } /* .map name of the mission used in cache filename and savegame.bin     */
+    public string Description { get; set; } /* human-readable description/name of the map, e.g. Lumoria C & D       */
+  }
+}
