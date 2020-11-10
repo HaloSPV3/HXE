be4632075d4037af7cd8e5e287c6195855ac923b
Noah Sherwin <noahrsherwin@gmail.com>
2020-11-10 01:28:01 -0800

    Begin adding 'Search for MCC CEA' feature
    This commit was amended several time before I felt comfortable with it as a base to work upon.
    
    Notable Additions:
    * class     HXE.Paths.Steam
    * class     HXE.Steam.Libraries
    * class     HXE.Steam.MCC
    * class     HXE.Steam.MCC.Halo1
    * function  HXE.File.Length()
    * function  HXE.Steam.Libraries.ParseLibraries()
    * function  HXE.Steam.MCC.Halo1.VerifyHalo1DLL()
    * function  SPV3.Install.SteamEXEPath
    * namespace HXE.Steam

---
+++ src/Steam/Libraries.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HXE.Paths.MCC;
+
+namespace HXE.Steam
+{
+  class Libraries
+  {
+    /// <summary>
+    /// Read Steam's "libraryfolders.vdf" and assign each libary folder to an
+    /// entry in an index array. Then, walk each entry to find the given path.
+    /// </summary>
+    /// <remarks>
+    /// 
+    /// </remarks>
+    public void ParseLibraries(string path)
+    {
+      var file = (File) SteamLibs;
+      if (!file.Exists())
+        throw new Exception("Steam Library list not found.");
+      using (StreamReader reader = System.IO.File.OpenText(file))
+      {
+        int maxlines = 255; // arbitrary limit to save memory.
+        array string line[array] = { 0 };
+        while ((line = reader.ReadLine()) != null)
+        {
+          
+        }
+      }
+    }
+  }
+}
---
+++ src/Steam/MCC.Halo1.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using static HXE.Paths.MCC;
+
+namespace HXE.Steam
+{
+  partial class MCC
+  {
+    class Halo1
+    {
+      public string placeholder = "";
+
+      public bool VerifyDefaultPath()
+      {
+        return System.IO.File.Exists(Halo1dll);
+      }
+
+      /// <summary>
+      /// Check if the inferred Halo1.dll is probably legitimate.
+      /// </summary>
+      /// <returns>Returns true if the file is larger than 20MiB.</returns>
+      public bool VerifyHalo1DLL()
+      {
+        var fileinfo = new FileInfo(Halo1Path);
+        if (20971520 < (ulong)fileinfo.Length)
+            return true;
+        else return false;
+      }
+    }
+  }
+}
