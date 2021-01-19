a4e0a0d2a6b682860a18684151e7a2d043345c98
Noah Sherwin <noahrsherwin@gmail.com>
2021-01-19 02:22:17 -0800

    SPV3 Kernel and Loader improvements Simplify Resolution/ResolutionEnabled/Native bools Update Kernel/Loader bin files Improve Kernel/Loader sync
    HXE.Kernel
    * Renamed Resolution to Resolution Enabled.
    * Flipped Resolution logic to match new name.
    
    HXE.Paths
    * Increment kernel bin
    
    HXE.Settings
    * Renamed Resolution to Resolution Enabled.
    * Relabeled Resolution to AutoResolution and flipped logic.
    
    SPV3.Configuration
    * Sorted Save/Load function calls and assignments. That should resolve the resetting Loader settings.
    * Reworked ShowHxeSettings()' data sync. See SPV3.Kernel.
    
    SPV3.Configuration.Loader
    * Removed Native/_native variables. Swapped in ResolutionEnabled==false where necessary.
    * Updated a comment.
    
    SPV3.Configuration.Shaders
    * Removed local instance of HXE Kernel in favor of static SPV3.Kernel.hxe instance.
    
    SPV3.Kernel
    * Added CopyLoaderToKernel() and its counterpart, CopyKernelToLoader().
    * SPV3KernelOverrides() will now infer the kernel mode rather than assume SPV3.3.
      * HXE Sensor Tweak override has been merged to one line.
      * HXE Resolution Tweak is now assigned SPV3's equivalent.
    
    SPV3.MainLoad
    * Removed local instances of Kernel hxe/spv3 in favor of the static instances.
    * Updated references of Native to ResolutionEnabled and flipped logic.

--- src/Paths.cs
+++ src/Paths.cs
@@ -35,7 +35,7 @@ namespace HXE
     public const string Manifest   = "manifest.bin";
 
     public static readonly string Directory     = Combine(GetFolderPath(ApplicationData), "HXE");
-    public static readonly string Configuration = Combine(Directory,                      "kernel-0x02.bin");
+    public static readonly string Configuration = Combine(Directory,                      "kernel-0x03.bin");
     public static readonly string Exception     = Combine(Directory,                      "exception.log");
     public static readonly string Positions     = Combine(CurrentDirectory,               "positions.bin");
     public static readonly string DSOAL         = Combine(CurrentDirectory,               "dsoal-aldrv.dll");
