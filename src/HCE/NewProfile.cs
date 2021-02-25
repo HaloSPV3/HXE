9d0588467d3c7301268d52caec9d8e5d65a92f27
Noah Sherwin <noahrsherwin@gmail.com>
2021-02-25 03:34:03 -0800

    Begin adding unknown offsets and values to NewProfile's blam.sav creation Indicate shorten "buffer" sections of Profile.Offset

--- src/HCE/NewProfile.cs
+++ src/HCE/NewProfile.cs
@@ -44,11 +44,11 @@ namespace HXE
       bool isDir = System.IO.File.GetAttributes(path).HasFlag(FileAttributes.Directory);
 
       Debug(NewLine
-          + NewLine + $"Path is currently \"{path}\""
-          + NewLine + $"The Full Path is \"{Path.GetFullPath(path)}\""
-          + NewLine + $"Path points to a folder? {isDir}"
-          + NewLine + $"\"{path}\" exists?" + (isDir? Directory.Exists(path) : System.IO.File.Exists(path))
-          );
+        + NewLine + $"Path is currently \"{path}\""
+        + NewLine + $"The Full Path is \"{Path.GetFullPath(path)}\""
+        + NewLine + $"Path points to a folder? {isDir}"
+        + NewLine + $"\"{path}\" exists?" + (isDir? Directory.Exists(path) : System.IO.File.Exists(path))
+        );
     }
 
     /// <summary>
@@ -61,25 +61,37 @@ namespace HXE
     {
       Core("Creating Scaffold...");
 
-      /// Set Path to the -path parameter
-      /// Check if it exists, create it if it doesn't.
-      /// Print the full path to the console.
-      /// e.g. ".\temp\"
+      /** Scaffold Root
+       * Set Path to the -path parameter
+       * If it doesn't exist, create it.
+       * Print the full path to the console.
+       * e.g. ".\temp\"
+       */
       var file = (File) path;
       Directory.CreateDirectory(file.Path);
       VerifyPath(file.Path);
 
-      /// Set Path to the savegames directory, et cetera
-      file.Path = Custom.Profiles(path);  /// e.g. ".\temp\savegames\
+      /** Savegames
+       * Set Path to the savegames directory
+       * If it doesn't exist, create it.
+       * e.g. ".\temp\savegames\
+       */
+      file.Path = Custom.Profiles(path);
       Directory.CreateDirectory(file.Path);
       VerifyPath(file.Path);
 
-      /// Set Path to the current Profile's directory, et cetera
+      /** Profile's folder
+       * Set Path to the current Profile's directory, et cetera
+       * If it doesn't exist, create it.
+       */
       file.Path = Custom.ProfileDirectory(path, profile.Details.Name);  /// e.g. ".\temp\savegames\New001\"
       Directory.CreateDirectory(file.Path);
       VerifyPath(file.Path);
 
-      /// Create blam.sav
+      /** Profile's Blam.sav
+       * Create blam.sav
+       * <see cref="Profile.Offset"/>
+       */
       using (var fs = new FileStream(Custom.Profile(path, profile.Details.Name), FileMode.OpenOrCreate)) /// e.g. ".\temp\savegames\New001\blam.sav"
       using (var ms = new MemoryStream(8192))
       using (var bw = new BinaryWriter(ms))
@@ -89,7 +101,7 @@ namespace HXE
 
         /** Write Buffer 1 (7fff) */
         {
-          fs.Position = 0x13c;
+          ms.Position = 0x13c;
           while (ms.Position < 0x938)
           {
             bw.Write(0xff7f); // 0x7fff BE
@@ -97,33 +109,83 @@ namespace HXE
           }
         }
 
+        /** Unknown bits */
+        {
+          ms.Position = 0x11a;
+          bw.Write(0xffff);
+
+          ms.Position = 0x12e;
+          bw.Write(0x0300); // 0x3 BE
+
+          ms.Position = 0x134;
+          bw.Write(0x0900); // 0x9
+          ms.Position = 0x136;
+          bw.Write(0x0c00); // 0xC
+          ms.Position = 0x138;
+          bw.Write(0x1b00); // 0x1B
+          ms.Position = 0x13a;
+          bw.Write(0x1c00); // 0x1C
+
+          ms.Position = 0x14e;
+          bw.Write(0x1200); // 0x12
+
+          ms.Position = 0x170;
+          bw.Write(0x0300); // 0x3
+          ms.Position = 0x172;
+          bw.Write(0x0500); // 0x5
+          ms.Position = 0x174;
+          bw.Write(0x1300); // 0x13
+          ms.Position = 0x176;
+          bw.Write(0x0200); // 0x2
+          ms.Position = 0x178;
+          bw.Write(0x0d00); // 0xD
+          ms.Position = 0x17a;
+          bw.Write(0x0f00); // 0xf
+          ms.Position = 0x17c;
+          bw.Write(0x1000); // 0x10
+
+          ms.Position = 0x18e;
+          bw.Write(0x1500); // 0x15
+          ms.Position = 0x190;
+          bw.Write(0x1400); // 0x14
+          ms.Position = 0x192;
+
+
+
+        }
+
         ms.Position = 0;
         ms.CopyTo(fs);
       }
       VerifyPath(Custom.Profile(path, profile.Details.Name));
 
-      /// Create savegame.bin
-      file.Path = Custom.Progress(path, profile.Details.Name); /// e.g. ".\temp\savegames\New001\savegame.bin"
+      /** Savegame.bin
+       * Create savegame.bin 
+       * e.g. ".\temp\savegames\New001\savegame.bin"
+       */
+      file.Path = Custom.Progress(path, profile.Details.Name);
       file.WriteAllBytes(new byte[0x480000]); /// 0x480000 == int 4718592
       VerifyPath(file.Path);
 
-      /// Create waypoint
-      file.Path = Custom.Waypoint(path, profile.Details.Name); /// e.g. ".\temp\savegames\New001\New001"
+      /** Profile file (Waypoint)
+       * Create waypoint 
+       * e.g. ".\temp\savegames\New001\New001"
+       */
+      file.Path = Custom.Waypoint(path, profile.Details.Name);
       file.WriteAllText(Custom.Waypoint(path, profile.Details.Name));
       VerifyPath(file.Path);
-      /// "waypoint" is used to refer to the file at "Path\\savegames\\Name\\Name"
-      /// Halo writes the path of this file as the file's contents.
-      /// For instance, `.\profiles\savegames\New001\New001`
-      ///   when `-path .\profiles`
 
-      /// Create or overwrite lastprof.txt
+      /** lastprof.txt
+       * Create or overwrite lastprof.txt 
+       * e.g. ".\temp\lastprof.txt"
+       */
       file.Path = Custom.LastProfile(path);
       file.WriteAllBytes(new byte[0xFF]); /// 255 int. Makes room for up to 255 characters.
       lastprofile.Path = file.Path;
       lastprofile.Save();
       VerifyPath(file.Path);
 
-      Info("Scaffold created.");
+      Info("Scaffold Completed.");
     }
   }
 }
