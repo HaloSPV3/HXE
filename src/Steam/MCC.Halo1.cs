63ae5505e80c229e39d16713fa3b43241d776708
Noah Sherwin <noahrsherwin@gmail.com>
2021-03-30 05:05:38 -0700

    (WIP) Add Certificate Validation for Halo1.dll It's very reluctant to have us load its assmebly data.
    Exception thrown: 'System.BadImageFormatException' in mscorlib.dll
    Could not load file or assembly 'file:///J:\Games\Steam\steamapps\common\Halo The Master Chief Collection\Halo1\halo1.dll' or one of its dependencies. The module was expected to contain an assembly manifest.

--- src/Steam/MCC.Halo1.cs
+++ src/Steam/MCC.Halo1.cs
@@ -1,15 +1,15 @@
 ﻿/**
  * Copyright (c) 2019 Emilian Roman
  * Copyright (c) 2020 Noah Sherwin
- * 
+ *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
  * arising from the use of this software.
- * 
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
- * 
+ *
  * 1. The origin of this software must not be misrepresented; you must not
  *    claim that you wrote the original software. If you use this software
  *    in a product, an acknowledgment in the product documentation would be
@@ -20,6 +20,11 @@
  */
 
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Cache;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using static HXE.Paths.MCC;
 
 namespace HXE.Steam
@@ -29,18 +34,31 @@ namespace HXE.Steam
     public static class Halo1
     {
       /// <summary>
-      ///   Set a new path for Halo1.dll
+      /// Set a new path for Halo1.dll
       /// </summary>
-      public static void SetHalo1Path()
+      public static void SetHalo1Path(Platform platform)
       {
-        var libs = new Libraries();
-        var mccH1 = Path.Combine(SteamMccH1, Halo1dll);
+        switch(platform)
+        {
+          case Platform.Steam:
+            var libraries = new Libraries();
+            var mccH1 = Path.Combine(HTMCC, Halo1dir, Halo1dll);
+            libraries.ParseLibraries();
+            Halo1Path = libraries.FindInLibraries(mccH1).First();
+            break;
+          case Platform.WinStore:
+            // TODO
+            throw new System.NotImplementedException("TODO: Add function to find WinStore MCC files.");
+            //break;
+          default:
+            throw new System.ArgumentOutOfRangeException(
+              $"Cannot set Halo1.dll path: Specified platform is invalid." + System.Environment.NewLine +
+              $"How did you do that?");
+        }
 
-        libs.ParseLibraries();
-        libs.ScanLibraries(mccH1);
-        Halo1Path = libs.ReturnPaths[0];
-        if (Halo1Path == null) 
+        if (Halo1Path == null)
           throw new FileNotFoundException("Halo1.dll not found");
+
         if (!VerifyHalo1DLL())
           throw new System.Exception("Halo1.dll is invalid.");
       }
@@ -48,13 +66,76 @@ namespace HXE.Steam
       /// <summary>
       /// Check if the inferred Halo1.dll is probably legitimate.
       /// </summary>
-      /// <returns>Returns true if the file is larger than 20MiB.</returns>
+      /// <returns>
+      /// Returns true if the file's Digital Certificate is valid.
+      /// </returns>
       public static bool VerifyHalo1DLL()
       {
-        var fileinfo = new FileInfo(Halo1Path);
-        if (20971520 < (ulong)fileinfo.Length)
-            return true;
-        else return false;
+        var assembly = Assembly.GetExecutingAssembly();
+
+        /** Convert embedded resource to X509Certificate */
+        var P7B_Fallback = new X509Certificate();
+        {
+          var ms = new MemoryStream();
+          assembly.GetManifestResourceStream(@"HXE.Assets.343I_DER.cer").CopyTo(ms);
+          var d = ms.ToArray();
+          P7B_Fallback = new X509Certificate(d);
+        }
+
+
+        /** Get 343 Industries' Public Key from internet source. */
+        // TODO: Add URI for web-accessible public key
+        var uri = string.Empty;
+        X509Certificate remoteCert;
+        try
+        {
+          remoteCert = new X509Certificate((GetWebStream(uri) as MemoryStream).ToArray());
+        }
+        catch(System.Exception)
+        {
+          remoteCert = null;
+        }
+
+        /** Get Public Key of reference Certificate.
+         * If we failed to get the remote certificate,
+         * fallback to the embedded resource.
+         */
+        var publicKey = remoteCert == null ?
+            P7B_Fallback.GetPublicKey() :
+            remoteCert.GetPublicKey();
+
+        /** Get certificate from local Halo1.dll. */
+
+        var file = Assembly.LoadFrom(Halo1Path);
+        Module module = file.GetModules().First();
+        var cert = module.GetSignerCertificate();
+
+        /** Return whether or not Halo1.dll is valid.
+         * If the Halo1.dll certificate's public key is equal to
+         * the cached PublicKey, return True. */
+        return cert.GetPublicKey() == publicKey;
+      }
+
+      public enum Platform
+      {
+        Steam,
+        WinStore
+      }
+
+      public static Stream GetWebStream(string uri)
+      {
+        Stream dataStream;
+        var webRequest = WebRequest.Create(uri);
+        webRequest.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.NoCacheNoStore);
+
+        using (var wr = (HttpWebResponse) webRequest.GetResponse())
+        using (var rs = wr.GetResponseStream())
+        using (var sr = new StreamReader(rs ?? throw new System.NullReferenceException("No response.")))
+        {
+          dataStream = sr.BaseStream;
+        }
+
+        return dataStream;
       }
     }
   }
