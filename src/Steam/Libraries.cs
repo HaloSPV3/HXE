d71a15cd971401625f3874611293f08072e4d7d9
Noah Sherwin <noahrsherwin@gmail.com>
2021-03-30 05:01:40 -0700

    Clean up the Steam Libraries class

--- src/Steam/Libraries.cs
+++ src/Steam/Libraries.cs
@@ -1,15 +1,15 @@
 ﻿/**
  * Copyright (c) 2019 Emilian Roman
  * Copyright (c) 2020 Noah Sherwin
- * 
+ *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
  * arising from the use of this software.
- * 
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
- * 
+ *
  * 1. The origin of this software must not be misrepresented; you must not
  *    claim that you wrote the original software. If you use this software
  *    in a product, an acknowledgment in the product documentation would be
@@ -19,9 +19,10 @@
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using static System.IO.Path;
-using static HXE.Paths.MCC;
 
 namespace HXE.Steam
 {
@@ -31,10 +32,10 @@ namespace HXE.Steam
     /// Create an object representing LibraryFolders.vdf on the file system.
     /// Local functions will read the contents of this file object.
     /// </summary>
-    public File     LibFoldersVdf = (File)SteamLibList;
-    public string[] LibList       = new string[15];  /// Arbitrary limit of 16 libraries. If you go over this, you're insane.
-    public string[] ReturnPaths   = new string[15]; /// Arbitrary limit of 16 results per search.
-    
+    public File         LibFoldersVdf = (File) Paths.Steam.Libraries;
+    public List<string> LibList       = new List<string>();  /// Arbitrary limit of 16 libraries. If you go over this, you're insane.
+    public List<string> ReturnPaths   = new List<string>(); /// Arbitrary limit of 16 results per search.
+
     /// <summary>
     /// Read Steam's "libraryfolders.vdf" and assign the library folders to an index array.
     /// </summary>
@@ -42,32 +43,14 @@ namespace HXE.Steam
     {
       if (!LibFoldersVdf.Exists() || LibFoldersVdf.Path == null)
         throw new FileNotFoundException("Steam Library list not found.");
-      string allText = LibFoldersVdf.ReadAllText();
-      string[] lines = allText.Split('\n');
-      ushort x = 0; 
-      ushort y = 1;
-      ushort z = (ushort)lines.GetUpperBound(0);
 
-      /// <example>
-      /// 
-      /// </example>
+      var text = LibFoldersVdf.ReadAllText();
 
-      while (lines[x] != null)
+      List<string> libs = text.Split(new char[] { '\"' }, System.StringSplitOptions.RemoveEmptyEntries).ToList();
+      libs = libs.Where(line => line.Contains(":")).ToList();
+      foreach(string line in libs)
       {
-        /// extract the path string from the line if the line matches a pattern.
-        if (lines[x].Contains($"\"{y}\"")) /// e.g. "1" OR "2" OR "3"
-        {
-          lines[x] = lines[x].Trim('\t'); /// Trim Tab characters
-          lines[x] = lines[x].Replace($"\"{y}\"", "");
-          lines[x] = lines[x].Replace("\"", "");
-          lines[x] = lines[x].Replace("\\\\", "\\");
-          lines[x] = lines[x].Trim('\t');
-          LibList[y - 1] = lines[x]; /// y-1 because 0 is unused. This moves all entries so the first one occupies LibList[0]
-          y++;
-        }
-        if (x != z)
-          x++;
-        else lines[x] = null;
+        LibList.Add(line.Replace("\\\\", "\\"));
       }
     }
 
@@ -85,34 +68,30 @@ namespace HXE.Steam
     }
 
     /// <summary>
-    /// Search each discovered Steam Library for a given path. Each result is assigned to the ReturnPaths array. 
+    /// Search each discovered Steam Library for a given path. <br/>
+    /// Each result is assigned to the ReturnPaths array.
     /// </summary>
-    /// <param name="path">The path(s) to find within the Steam Library folders.</param>
+    /// <param name="pathInLibrary">
+    /// The path to find within the Steam Library folders.
+    /// </param>
     /// <remarks>
-    /// If only one result is expected, it can be accessed as ReturnPath[0].
+    /// If only one result is expected, it can be accessed as ReturnPath[0]. <br/>
     /// If there are multiple results, implement another While loop to filter them.
-    /// 
     /// </remarks>
-    public void ScanLibraries(string path)
+    public List<string> FindInLibraries(string pathInLibrary)
     {
-      int x = 0; /// LibList index
-      int y = 0; /// ReturnPaths index
-      int z = LibList.GetUpperBound(0);
-
-      while (x != 15 && LibList[x] != null)
+      List<string> list = new List<string>();
+      foreach(string library in LibList)
       {
-        File file = (File)LibList[x]; /// each element in LibList[] is assigned as a File object's Path.
-        file.Path = Combine(file.Path, path);
-        if (file.Exists())
-        {
-          ReturnPaths[y] = file.Path;
-          y++;
-        }
-        if (x != z)
-          x++;
-        else
-          LibList[x] = null;
+        string path = Combine(library, "steamapps", "common", pathInLibrary);
+        if(System.IO.File.Exists(path))
+          list.Add(path);
       }
+
+      if(list.Count == 0)
+        throw new System.Exception("Failed to find path in Steam Libraries.");
+
+      return list;
     }
   }
 }
