f854c4aa94583a7ff8fa440363659a0d800f6ab1
Noah Sherwin <noahrsherwin@gmail.com>
2021-04-02 21:29:16 -0700

    Polish VerifyHalo1DLL()
    Try to optimize task usage of CheckSteamPath().

--- src/Steam/MCC.Halo1.cs
+++ src/Steam/MCC.Halo1.cs
@@ -75,29 +75,56 @@ namespace HXE.Steam
       {
         /// Known issue: This doesn't verify the file is a real Halo1.dll
         /// It merely checks if the file's certificate is valid.
+        ///
+        /// Tip:
+        /// If getting the data for 'P7B_Fallback' and 'remoteCert'
+        /// took >200ms for each to complete, we would move them to
+        /// new methods/functions as Async Tasks so they can be
+        /// executed simultaneously and wait for both to complete
+        /// (successfully or not) before the last line which compares
+        /// one of those certificates to Halo1.dll's certificate.
+        ///
+        /// However, only the remoteCert takes a significant amount of time
+        /// to complete and it has a 200ms timeout to mitigate wait time.
 
         /** Convert embedded resource to X509Certificate */
         X509Certificate P7B_Fallback;
+        try
         {
           var ms = new MemoryStream();
           var assembly = Assembly.GetExecutingAssembly();
-          assembly.GetManifestResourceStream(@"HXE.Assets.343I_DER.cer").CopyTo(ms);
+          assembly.GetManifestResourceStream(@"HXE.Assets.343I_DER.cer")
+            .CopyTo(ms);
           P7B_Fallback = new X509Certificate(ms.ToArray());
+        }
+        catch (Exception e)
+        {
+          throw new Exception("Failed to read our embedded copy of 343 Inudstries' code certificate",
+            e);
         }
 
         /** Get 343 Industries' Public Key from internet source. */
         // TODO: Add OFFICIAL URI for web-accessible public key
         var uri = "https://github.com/HaloSPV3/HCE/releases/download/updates/343I_DER.exp2022-04-27.cer";
-        X509Certificate remoteCert = new X509Certificate();
-        if (!string.IsNullOrEmpty(uri))
-          remoteCert = new X509Certificate(GetWebStream(uri).ToArray());
+        var remoteCert = new X509Certificate();
+        var remoteFailedOrTimedOut = false;
+        try
+        {
+          MemoryStream ms = GetWebStream(uri);
+          byte[] msArray  = ms.ToArray();
+          remoteCert      = new X509Certificate(msArray);
+        }
+        catch (Exception)
+        {
+          remoteFailedOrTimedOut = true;
+        }
 
         /** Get Public Key of a reference Certificate.
          * If we failed to get the remote certificate,
          * fallback to the embedded resource.
          * This allows for offline validation.
          */
-        var publicKey_ref = string.IsNullOrEmpty(uri) ?
+        var publicKey_ref = remoteFailedOrTimedOut ?
             P7B_Fallback.GetPublicKey() :
             remoteCert.GetPublicKey();
 
@@ -121,6 +148,7 @@ namespace HXE.Steam
         MemoryStream dataStream = new MemoryStream();
         var webRequest = WebRequest.Create(uri);
         webRequest.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.NoCacheNoStore);
+        webRequest.Timeout = 200; // in milliseconds
 
         using (var wr = (HttpWebResponse) webRequest.GetResponse())
         using (var rs = wr.GetResponseStream())
