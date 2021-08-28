f89487e3c4e40a1e8ea1d22fdade04be2885a926
Noah Sherwin <noahrsherwin@gmail.com>
2021-08-28 00:04:23 -0700

    build: add prebuild powershell script
    Currently, this script ensures the requirements have been met to build with GitVersion.

---
+++ src/.msb.prebuild.ps1
@@ -0,0 +1,12 @@
+﻿# Git - Ensure we can execute Git
+try {
+  Write-Host "Git was found. It is $(git --version) at $(where.exe git)"
+}
+catch {
+  Write-Error "Git is not installed or it isn't in the environment variable PATH!";
+}
+
+# GitVersion - Ensure unshallow checkout
+if ('true' -eq $(git rev-parse --is-shallow-repository)) {
+  git fetch --unshallow;
+}
