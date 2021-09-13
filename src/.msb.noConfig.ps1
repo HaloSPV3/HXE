8c2bce473ec0c811388cb1424feff91e9212d191
Noah Sherwin <noahrsherwin@gmail.com>
2021-09-13 00:22:00 -0700

    chore: refine noConfig PS script
    Always ensure the expected working directory is set.
    Save and later restore the original working directory.

--- src/.msb.noConfig.ps1
+++ src/.msb.noConfig.ps1
@@ -1,2 +1,6 @@
-Set-Location ..\bin
+$originalPath = $(Get-Location).Path
+
+Set-Location $PSScriptRoot\..\bin
 Get-ChildItem -Path "HXE.exe.config" -Recurse | Remove-Item
+
+Set-Location $originalPath
