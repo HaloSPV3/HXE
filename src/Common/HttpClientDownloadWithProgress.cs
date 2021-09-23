3d99bad72d3d93f1ed617e7538e109dd1dd477d2
Noah Sherwin <noahrsherwin@gmail.com>
2021-09-23 04:12:15 -0700

    refactor: rename ProgressChangedHandler to DownloadProgressHandler

--- src/Common/HttpClientDownloadWithProgress.cs
+++ src/Common/HttpClientDownloadWithProgress.cs
@@ -16,7 +16,7 @@ namespace System.Net.Http
 
         public delegate void ProgressChangedHandler(long? totalFileSize, long totalBytesDownloaded, double? progressPercentage);
 
-        public event ProgressChangedHandler ProgressChanged;
+        public event DownloadProgressHandler ProgressChanged;
 
         public HttpClientDownloadWithProgress(string downloadUrl, string destinationFilePath, CancellationToken? cancellationToken = null)
         {
