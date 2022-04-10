a7ec42f046cdf572db5351334cf6f266225f3db2
Noah Sherwin <noahrsherwin@gmail.com>
2022-04-10 03:41:31 -0700

    fix: get FileSystemCompression working
    - replace Constants class with CsWin32 constants
    - update UpdateProgress
    - set SeBackupPrivilege
    - get Directory handles properly
    - add FileInfo.GetHandle()
    - add DirectoryInfo.GetHandle()
    - rewrite SetCompression() to use CsWin32
    - Add ThrowWin32Exception()

--- src/Common/FileSystemCompression.cs
+++ src/Common/FileSystemCompression.cs
@@ -21,22 +21,18 @@
 /// https://stackoverflow.com/a/624446
 /// - Zack Elan https://stackoverflow.com/users/2461/zack-elan
 /// - Goz https://stackoverflow.com/users/131140/goz
-
 using System;
 using System.IO;
-using System.Runtime.InteropServices;
+using HXE.Extensions;
+using Microsoft.Win32.SafeHandles;
 using PInvoke;
+using Windows.Win32.Storage.FileSystem;
+using static Windows.Win32.PInvoke;
 
 namespace HXE.Common
 {
     internal static class FileSystemCompression
     {
-        private static class Constants
-        {
-            public const int FSCTL_SET_COMPRESSION = 0x9C040;
-            public const short COMPRESSION_FORMAT_DEFAULT = 1;
-        }
-
         // TODO: Duplicate as non-extension "CompressDirectory()"
         /// <summary>
         ///     Compress the directory represented by the DirectoryInfo object.
@@ -57,14 +53,14 @@ namespace HXE.Common
         {
             /* Progress */
             bool withProgress = progress != null;
-            var status = withProgress ? new Status
+            Status status = withProgress ? new Status
             {
                 Description = $"Compressing '{directoryInfo.FullName}' and its descendents...",
                 Current = 0,
                 Total = 1
             } : null;
 
-            void UpdateProgress(int n)
+            void UpdateProgress(int n = 1)
             {
                 if (!withProgress) return; // not necessary, but it's here just in case.
                 status.Current += n;
@@ -97,21 +93,17 @@ namespace HXE.Common
                 UpdateProgress(0);
             }
 
-            /* Compress root directory */
-            Kernel32.SafeObjectHandle hDirectory = Kernel32.CreateFile(
-                filename: directoryInfo.FullName,
-                access: Kernel32.ACCESS_MASK.GenericRight.GENERIC_READ | Kernel32.ACCESS_MASK.GenericRight.GENERIC_WRITE,
-                share: Kernel32.FileShare.None,
-                securityAttributes: Kernel32.SECURITY_ATTRIBUTES.Create(),
-                creationDisposition: Kernel32.CreationDisposition.OPEN_EXISTING,
-                flagsAndAttributes: Kernel32.CreateFileFlags.FILE_FLAG_BACKUP_SEMANTICS,
-                templateFile: null
-            );
+            /* Adjust current process permissions */
+            System.Diagnostics.Process.GetCurrentProcess().SetSeBackupPrivilege();
 
-            SetCompression(hDirectory);
-            hDirectory.Close();
+            /* Compress root directory */
+
+            SafeFileHandle directoryHandle = directoryInfo.GetHandle();
+
+            SetCompression(directoryHandle);
+            directoryHandle.Dispose();
             if (withProgress)
-                UpdateProgress(1);
+                UpdateProgress();
 
             /* Compress sub-directories */
             if (recurse)
@@ -120,7 +112,7 @@ namespace HXE.Common
                 {
                     directory.Compress();
                     if (withProgress)
-                        UpdateProgress(1);
+                        UpdateProgress();
                 }
             }
 
@@ -131,7 +123,7 @@ namespace HXE.Common
                 {
                     file.Compress();
                     if (withProgress)
-                        UpdateProgress(1);
+                        UpdateProgress();
                 }
             }
         }
@@ -149,8 +141,63 @@ namespace HXE.Common
         public static void Compress(this FileInfo fileInfo)
         {
             FileStream fileStream = fileInfo.Open(mode: FileMode.Open, access: FileAccess.ReadWrite, share: FileShare.None);
-            SetCompression((Kernel32.SafeObjectHandle)(SafeHandle)fileStream.SafeFileHandle);
+            SetCompression(fileStream.SafeFileHandle);
             fileStream.Dispose();
+        }
+
+
+        public static SafeFileHandle GetHandle(this DirectoryInfo directoryInfo)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().SetSeBackupPrivilege();
+
+            SafeFileHandle directoryHandle = CreateFile(
+                lpFileName: directoryInfo.FullName,
+                dwDesiredAccess: FILE_ACCESS_FLAGS.FILE_GENERIC_READ | FILE_ACCESS_FLAGS.FILE_GENERIC_WRITE,
+                dwShareMode: FILE_SHARE_MODE.FILE_SHARE_NONE,
+                lpSecurityAttributes: null,
+                dwCreationDisposition: FILE_CREATION_DISPOSITION.OPEN_EXISTING,
+                dwFlagsAndAttributes: FILE_FLAGS_AND_ATTRIBUTES.FILE_FLAG_BACKUP_SEMANTICS,
+                hTemplateFile: null
+                );
+
+            if (directoryHandle.IsInvalid)
+            {
+                var error = Kernel32.GetLastError();
+                switch (error)
+                {
+                    case Win32ErrorCode.ERROR_SHARING_VIOLATION: break;
+                }
+                /// TODO: Handle the following exceptions:
+                /// PInvoke.Win32ErrorCode.ERROR_SHARING_VIOLATION
+                ///   The process cannot access the file because it is being used by another process
+                /// Win32ErrorCode.ERROR_CANT_ACCESS_FILE ???? maybe ACLs
+                /// Win32ErrorCode.ERROR_FILE_CHECKED_OUT ????
+                /// Win32ErrorCode.ERROR_FILE_ENCRYPTED ???? just skip the entry
+                /// Win32ErrorCode.ERROR_FILE_INVALID ????
+                /// Win32ErrorCode.ERROR_FILE_NOT_FOUND ???? After DirectoryInfo instance is created, the fs item may be deleted
+                /// Win32ErrorCode.ERROR_FILE_OFFLINE ???? for network drives, offline files
+                /// Win32ErrorCode.ERROR_FILE_READ_ONLY ???? try temporary disable
+                /// Win32ErrorCode.ERROR_FILE_SYSTEM_LIMITATION ???? check for NTFS!
+                /// Win32ErrorCode.ERROR_FILE_TOO_LARGE ???? Only files smaller than 30 GiB can be compressed!
+                /// Win32ErrorCode.ERROR_OPEN_FILES ????
+                /// Win32ErrorCode.ERROR_TOO_MANY_OPEN_FILES ???? Very rare, but possible
+                /// Win32ErrorCode.ERROR_USER_MAPPED_FILE ???? part of the file is open. not sure if problem
+                ThrowWin32Exception(error);
+                return new SafeFileHandle(IntPtr.Zero, true);
+            }
+            else
+            {
+                return directoryHandle;
+            }
+        }
+
+        public static SafeFileHandle GetHandle(this FileInfo fileInfo)
+        {
+            FileStream fileStream = fileInfo.Open(
+                FileMode.Open,
+                FileAccess.ReadWrite,
+                FileShare.None);
+            return fileStream.SafeFileHandle;
         }
 
         /// <summary>
@@ -158,24 +205,31 @@ namespace HXE.Common
         /// </summary>
         /// <param name="handle"></param> //TODO
         /// <exception cref="Win32Exception">DeviceIoControl operation failed. See <see cref="Win32Exception.NativeErrorCode"/> for exception data.</exception>
-        public static unsafe void SetCompression(Kernel32.SafeObjectHandle handle)
+        internal static unsafe void SetCompression(SafeFileHandle handle)
         {
-            short lpInBuffer = Constants.COMPRESSION_FORMAT_DEFAULT;
-            bool success = Kernel32.DeviceIoControl(
-                hDevice: handle,
-                dwIoControlCode: Constants.FSCTL_SET_COMPRESSION,
-                inBuffer: &lpInBuffer,
-                nInBufferSize: sizeof(short), // sizeof(typeof(lpInBuffer.GetType()))
-                outBuffer: (void*)IntPtr.Zero,
-                nOutBufferSize: 0,
-                pBytesReturned: out _,
-                lpOverlapped: (Kernel32.OVERLAPPED*)IntPtr.Zero
-                );
+            uint defaultFormat = COMPRESSION_FORMAT_DEFAULT;
 
-            if (!success)
+            if (!DeviceIoControl(
+                hDevice: handle,
+                dwIoControlCode: FSCTL_SET_COMPRESSION,
+                lpInBuffer: &defaultFormat,
+                nInBufferSize: sizeof(uint), // sizeof(typeof(lpInBuffer.GetType()))
+                lpOutBuffer: (void*)IntPtr.Zero,
+                nOutBufferSize: 0,
+                lpBytesReturned: (uint*)IntPtr.Zero,
+                lpOverlapped: (Windows.Win32.System.IO.OVERLAPPED*)IntPtr.Zero
+                ))
             {
-                throw new Win32Exception(Kernel32.GetLastError());
+                ThrowWin32Exception(Kernel32.GetLastError());
             }
+        }
+
+        internal static void ThrowWin32Exception(Win32ErrorCode errorCode, string messageAppendix = null)
+        {
+            throw new Win32Exception(
+                error: errorCode,
+                message: errorCode.GetMessage() + " : " + messageAppendix
+            );
         }
     }
 }
