a8f0cf785ceb87c93e6c98a9d51fbdd0d828bff9
Noah Sherwin <noahrsherwin@gmail.com>
2023-06-19 05:00:21 -0700

    refactor: change MainWindow ctor to expression

--- src/MainWindow.axaml.cs
+++ src/MainWindow.axaml.cs
@@ -4,8 +4,5 @@ namespace HXE;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+    public MainWindow() => InitializeComponent();
 }
