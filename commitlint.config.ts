d9df1df211858cf73679233a72ea43655000e2ba
Noah Sherwin <noahrsherwin@gmail.com>
2026-04-06 20:47:40 -0700

    chore(vscode): set .vscode/*json to JSONC

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,9 +2,9 @@ import baseConfig from '@halospv3/hce.shared-config/commitlintConfig';
 
 const scopes = {
   contributing: 'Affects CONTRIBUTING.md',
-  deps: 'Affects dependencies bundled with or depended on by published packages and artifacts. ' +
-    'For NuGet package/PackageReferences, this means anything that has "runtime", "native" or ' +
-    '"contentfiles" included.',
+  deps: 'Affects dependencies bundled with or depended on by published packages and artifacts. '
+    + 'For NuGet package/PackageReferences, this means anything that has "runtime", "native" or '
+    + '"contentfiles" included.',
   'deps-dev': 'Affects dependencies used by CI, dev environments, or the build system(s); '
     + 'but are not required at runtime nor bundled with or statically linked into the published '
     + 'binaries or packages. For NuGet packages/PackageReferences, this would be anything with'
