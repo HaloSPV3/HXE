a48b4cf9d01e0607653b4e04ef7b51d768f961fe
Noah Sherwin <noahrsherwin@gmail.com>
2026-07-26 03:13:00 -0700

    fix(Net): resolve HttpClient crash; decrease timeout from insane 24 hours to runtime-default 30 seconds

--- src/Net/DefaultHttpClient.cs
+++ src/Net/DefaultHttpClient.cs
@@ -6,6 +6,6 @@ namespace HXE.Net
     public static class DefaultHttpClient
     {
         public static HttpClient Client { get; } = new HttpClient { Timeout = DEFAULT_TIMEOUT };
-        public static readonly TimeSpan DEFAULT_TIMEOUT = TimeSpan.FromDays(1);
+        public static readonly TimeSpan DEFAULT_TIMEOUT = TimeSpan.FromSeconds(30);
     }
 }
