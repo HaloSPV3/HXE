7518948286f5d3b8cba0211eac4e5bd4e1cdce61
Noah Sherwin <noahrsherwin@gmail.com>
2026-08-03 04:49:41 -0700

    fix(Net): initialize `DefaultHttpClient` in a way that doesn't cause a crash immediately

--- src/Net/DefaultHttpClient.cs
+++ src/Net/DefaultHttpClient.cs
@@ -5,7 +5,18 @@ namespace HXE.Net
 {
     public static class DefaultHttpClient
     {
-        public static HttpClient Client { get; } = new HttpClient { Timeout = DEFAULT_TIMEOUT };
-        public static readonly TimeSpan DEFAULT_TIMEOUT = TimeSpan.FromSeconds(30);
+        static DefaultHttpClient()
+        {
+            // the runtime does not allow reading a field/property before exiting the constructor;
+            // (The resulting OutOfRangeException was misleading and took a few evenings to debug)
+            // To instantiate-once-assign-twice, one must assign the instance to a temporary variable
+            // and use that variable for the other assignments.
+            var defaultTimeout = TimeSpan.FromSeconds(30);
+            DEFAULT_TIMEOUT = defaultTimeout;
+            Client = new HttpClient()
+            { Timeout = defaultTimeout };
+        }
+        public static HttpClient Client { get; }
+        public static readonly TimeSpan DEFAULT_TIMEOUT;
     }
 }
